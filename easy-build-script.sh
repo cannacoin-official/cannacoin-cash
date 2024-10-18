a56d6c1d4e6b31d87554c306eb829f925dbd01b4
deus-opus <deusopus@gmail.com>
2024-10-17 23:43:27 -0700

    Update easy-build-script.sh

--- easy-build-script.sh
+++ easy-build-script.sh
@@ -2,36 +2,12 @@
 
 # Update and install dependencies
 sudo apt update
-sudo apt upgrade -y
-sudo apt install -y build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git
-
-# Install Boost libraries
+sudo apt install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git
 sudo apt install -y libboost-all-dev
-
-# Install other necessary libraries
-sudo apt install -y libevent-dev libminiupnpc-dev libzmq3-dev
-
-# Remove any existing Berkeley DB packages (optional)
-sudo apt remove -y libdb-dev libdb++-dev
-
-# Install Berkeley DB 4.8
-cd ~
-# Download Berkeley DB 4.8 source code
-wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-
-# Extract the archive
-tar -xzvf db-4.8.30.NC.tar.gz
-
-# Build and install Berkeley DB 4.8
-cd db-4.8.30.NC/build_unix/
-# Configure the build with the appropriate prefix and options
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=/usr/local/BerkeleyDB.4.8
-# Compile and install
-make
-sudo make install
-
-# Set the Berkeley DB 4.8 environment variables
-export BDB_PREFIX='/usr/local/BerkeleyDB.4.8'
+sudo apt install -y libdb-dev libdb++-dev
+sudo apt install -y qtbase5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt install -y libqrencode-dev
+sudo apt install -y libminiupnpc-dev
 
 # Clone the Litecoin repository and rename it
 cd ~
@@ -64,7 +40,7 @@ sed -i 's/static const CAmount MAX_MONEY = 84000000 \* COIN;/static const CAmoun
 sed -i 's/CAmount nSubsidy = 50 \* COIN;/CAmount nSubsidy = 100 \* COIN;/g' src/validation.cpp
 
 # Create configuration directory and file
-mkdir -p ~/.cannacoincash
+mkdir ~/.cannacoincash
 echo "rpcuser=user" > ~/.cannacoincash/cannacoincash.conf
 echo "rpcpassword=pass" >> ~/.cannacoincash/cannacoincash.conf
 echo "rpcallowip=127.0.0.1" >> ~/.cannacoincash/cannacoincash.conf
@@ -72,15 +48,9 @@ echo "rpcport=9387" >> ~/.cannacoincash/cannacoincash.conf
 echo "server=1" >> ~/.cannacoincash/cannacoincash.conf
 echo "daemon=1" >> ~/.cannacoincash/cannacoincash.conf
 
-# Build CannacoinCash with Berkeley DB 4.8
+# Build the CannacoinCash software
 ./autogen.sh
-./configure LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/" \
-    --with-incompatible-bdb \
-    --disable-shared \
-    --enable-cxx \
-    --disable-tests \
-    --disable-bench
-
+./configure
 make
 
 # Start the CannacoinCash daemon
