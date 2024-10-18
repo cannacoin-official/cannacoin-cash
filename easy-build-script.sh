f71f2e2ed14d80cc7440dd900e0a845acbe81adf
deus-opus <deusopus@gmail.com>
2024-10-17 18:49:39 -0700

    Update and rename easy-setup-script.sh to easy-build-script.sh

--- easy-build-script.sh
+++ easy-build-script.sh
@@ -8,7 +8,6 @@ sudo apt install -y qtbase5-dev qttools5-dev-tools libprotobuf-dev protobuf-comp
 sudo apt install -y libqrencode-dev
 sudo apt install -y libminiupnpc-dev
 
-
 cd ~
 git clone https://github.com/litecoin-project/litecoin.git CannacoinCash
 cd CannacoinCash
@@ -20,21 +19,34 @@ find . -type f -exec sed -i 's/litecoin/cannacoincash/g' {} +
 find . -type f -exec sed -i 's/9333/9388/g' {} +  # New P2P Port
 find . -type f -exec sed -i 's/9332/9387/g' {} +  # New RPC Port
 
-# change magic bytes
+# Change magic bytes
 sed -i 's/0xdbb6c0fb/0xcacacaca/g' src/chainparams.cpp
 sed -i 's/0xfbc0b6db/0xcacacacb/g' src/chainparams.cpp
 sed -i 's/0xd9b4bef9/0xcacacacc/g' src/chainparams.cpp
 sed -i 's/0xdab5bffa/0xcacacacd/g' src/chainparams.cpp
+
+# Change the subsidy halving interval to 210,000 blocks
+sed -i 's/consensus.nSubsidyHalvingInterval = 840000;/consensus.nSubsidyHalvingInterval = 210000;/g' src/chainparams.cpp
+sed -i 's/static const int nSubsidyHalvingInterval = 840000;/static const int nSubsidyHalvingInterval = 210000;/g' src/consensus/consensus.h
+
+# Change the maximum money supply to 21 million coins
+sed -i 's/static const CAmount MAX_MONEY = 84000000 \* COIN;/static const CAmount MAX_MONEY = 21000000 \* COIN;/g' src/amount.h
+
+# Change the initial mining reward to 100 coins
+sed -i 's/CAmount nSubsidy = 50 \* COIN;/CAmount nSubsidy = 100 \* COIN;/g' src/validation.cpp
 
 mkdir ~/.cannacoincash
 echo "rpcuser=user" > ~/.cannacoincash/cannacoincash.conf
 echo "rpcpassword=pass" >> ~/.cannacoincash/cannacoincash.conf
 echo "server=1" >> ~/.cannacoincash/cannacoincash.conf
 
+# Build the CannacoinCash software
 ./autogen.sh
 ./configure
 make
 
+# Start the CannacoinCash daemon
 src/cannacoincashd -daemon
 
-echo "CannacoinCash succesfully forked and started"
+# Completion message
+echo "CannacoinCash successfully built and the daemon is now running."
